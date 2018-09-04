42ad7b55dfb4bfe6f0f533a11be1a8812a4a141c
Kevin Atteson <kevin@atteson.com>
2018-09-03 22:04:06 -0400

    moving to workstation

--- src/iterators.jl
+++ src/iterators.jl
@@ -6,9 +6,11 @@ struct BusinessDay <: DatePeriod
     value::Int
 end
 
-_units( bd::BusinessDay ) = string(bd.calendar) * " bday" * (abs(bd.value) == 1 ? "" : "s")
+units( bd::BusinessDay ) = string(bd.calendar) * " bday" * (abs(bd.value) == 1 ? "" : "s")
 
-Base.string( bd::BusinessDay ) = string(bd.value) * " " * _units(bd)
+Base.string( bd::BusinessDay ) = string(bd.value) * " " * units(bd)
+
+Base.show( io::IO, bd::BusinessDay ) = print( io, string(bd) )
 
 struct BusinessDayRange <: OrdinalRange{Date, BusinessDay}
     start::Date
@@ -17,6 +19,10 @@ struct BusinessDayRange <: OrdinalRange{Date, BusinessDay}
 end
 
 Base.string( r::BusinessDayRange ) = string(r.start) * ":(" * string(r.step) * "):" * string(r.stop)
+
+Base.print( io::IO, r::BusinessDayRange ) = print( io, string(r) )
+
+Base.show( io::IO, r::BusinessDayRange ) = print( io, string(r) )
 
 Base.iterate( r::BusinessDayRange, d::Date = advancebdays( r.step.calendar, r.start, 0 ) ) =
     let d1 = advancebdays( r.step.calendar, d, r.step.value )
--- test/testiterators.jl
+++ test/testiterators.jl
@@ -2,5 +2,10 @@ using Commas
 using Dates
 
 delta = Commas.BusinessDay( :USNYSE, 1 )
-range = Commas.BusinessDayRange( Date( 2017, 1, 1 ), delta, Date( 2018, 1, 1 ) )
+[Commas.BusinessDay( :USNYSE, i ) for i in 1:10]
+
+startdate = Date( 2017, 1, 1 )
+enddate = Date( 2018, 1, 1 )
+range = Commas.BusinessDayRange( startdate, delta, enddate )
+ranges = [Commas.BusinessDayRange( startdate, Commas.BusinessDay( :USNYSE, i ), enddate ) for i in 1:10]
 
