4d076c1d6163f02595fb64e08ec94588b5b40441
BuildTools <unconfigured@null.spigotmc.org>
2022-07-17 14:40:24 +0200

    misc

--- src/sort.jl
+++ src/sort.jl
@@ -2,11 +2,13 @@
 struct CountingSortAlg <: Base.Algorithm end
 
 function boundedcountmap(
-    v::AbstractVector{T};
+    v::AbstractVector{T},
+    lo::Int = firstindex(v),
+    hi::Int = lastindex(v);
     bound::Integer = 2 ^ 20,
 ) where {T}
     map = Dict{T,Int}()
-    for i = 1:length(v)
+    for i = lo:hi
         map[v[i]] = get( map, v[i], 0 ) + 1
         if length(map) > bound
             return (false,map)
@@ -15,30 +17,40 @@ function boundedcountmap(
     return (true,map)
 end
 
+# this is really sortperm!
 function Base.sort!(
-    startperm::AbstractVector{T},
+    startperm::AbstractVector{Int},
+    lo::Int,
+    hi::Int,
     ::CountingSortAlg,
-    o::Base.Perm,
-) where {T <: Integer}
+    o::Base.Perm{T,U},
+) where {T <: Base.Ordering, U <: AbstractVector}
+    # note that there is a special sorting algorithm for floats which conflicts with the above
     v = o.data
-    (nottoobig,cm) = boundedcountmap( v );
+    (nottoobig,cm) = boundedcountmap( v, lo, hi );
     if !nottoobig
-        return sortperm( startperm, MergeSort, o )
+        return sortperm!( startperm, lo, hi, MergeSort, o, initialized=true )
     end
     sks = sort(collect(keys(cm)))
     indices = Dict( zip( sks, [1;1 .+ cumsum( getindex.( [cm], sks ) )[1:end-1]] ) );
 
     n = length(v);
-    perm = zeros( T, n )
-    for i = 1:n
+    perm = zeros( Int, n )
+    for i = lo:hi
         x = v[startperm[i]]
         perm[indices[x]] = startperm[i]
         indices[x] += 1
     end
-    for i = 1:n
+    for i = lo:hi
         startperm[i] = perm[i]
     end
     return startperm
 end
 
+Base.sort!(
+    startperm::AbstractVector{Int},
+    alg::CountingSortAlg,
+    o::Base.Perm{T,U},
+) where {T <: Base.Ordering, V <: Union{Int,Date}, U <: AbstractVector{V}} =
+    sort!( startperm, firstindex(startperm), lastindex(startperm), alg, o )
     
--- test/testsort.jl
+++ test/testsort.jl
@@ -1,10 +1,59 @@
 using Commas
 using Random
 
+test( v, p, op ) = (u -> op( u[1:end-1], u[2:end] ))( v[p] )
+
 n = 1_000_000
 Random.seed!(1)
-v = rand( 1:10, n );
-perm = collect(1:n);
-@time sortperm!( perm, v, alg=Commas.CountingSortAlg() );
 
-issorted( v[perm] )
+
+v2 = rand( 1:10, n );
+perm = collect(1:n);
+@time sortperm!( perm, v2, alg=Commas.CountingSortAlg() );
+@assert( issorted( v2[perm] ) )
+
+v1 = rand( 11:100, n );
+@time sortperm!( perm, v1, alg=Commas.CountingSortAlg(), initialized=true );
+@assert( all(test( v1, perm, .< ) .| (test( v1, perm, .== ) .& test( v2, perm, .<= ))) )
+
+
+v2 = rand( n );
+perm = collect(1:n);
+@time sortperm!( perm, v2, alg=Commas.CountingSortAlg() );
+@assert( issorted( v2[perm] ) )
+
+v1 = rand( 1:100, n );
+@time sortperm!( perm, v1, alg=Commas.CountingSortAlg(), initialized=true );
+@assert( all(test( v1, perm, .< ) .| (test( v1, perm, .== ) .& test( v2, perm, .<= ))) )
+
+
+v2 = rand( 1:100, n );
+perm = collect(1:n);
+@time sortperm!( perm, v2, alg=Commas.CountingSortAlg() );
+@assert( issorted( v2[perm] ) )
+
+v1 = rand( n );
+@time sortperm!( perm, v1, alg=Commas.CountingSortAlg(), initialized=true );
+@assert( all(test( v1, perm, .< ) .| (test( v1, perm, .== ) .& test( v2, perm, .<= ))) )
+
+
+n = 10_000_000
+v2 = rand( 1:n, n );
+perm = collect(1:n);
+@time sortperm!( perm, v2, alg=Commas.CountingSortAlg() );
+@assert( issorted( v2[perm] ) )
+
+v1 = rand( 1:100, n );
+@time sortperm!( perm, v1, alg=Commas.CountingSortAlg(), initialized=true );
+@assert( all(test( v1, perm, .< ) .| (test( v1, perm, .== ) .& test( v2, perm, .<= ))) )
+
+
+v2 = rand( 1:100, n );
+perm = collect(1:n);
+@time sortperm!( perm, v2, alg=Commas.CountingSortAlg() );
+@assert( issorted( v2[perm] ) )
+
+v1 = rand( 1:n, n );
+@time sortperm!( perm, v1, alg=Commas.CountingSortAlg(), initialized=true );
+@assert( all(test( v1, perm, .< ) .| (test( v1, perm, .== ) .& test( v2, perm, .<= ))) )
+
